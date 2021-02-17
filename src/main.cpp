bae8b29a11174861cd5c1e289dbcc78b6ebf90f4
Ellis Stout <48662841+ElIzstout@users.noreply.github.com>
2021-02-17 09:08:13 -0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,23 @@
 #include <frc>
 using namespace frc;
 class Robot: public TimedRobot{
-  
+  //drive
   PWMVictorSPX::rightm(1);
   PWMVictorSPX::leftm(0);
-
   DifferentialDrive::robotdrive(leftm,rightm);
   XboxController::controller(1); //Lx=0 Ly=1 Lt=2 rt=3 Rx=4 ry=5 
   
-  teleopPeriodic(){
+  //pnematics
+  Compressor::compressor(); //the id might be wrong
+  
+ 
+  public void TeleopInit(){
+  compressor.start();
+  }
+  public void TeleopPeriodic(){
     
     robotdrive.ArcadeDrive(controller.GetRawAxis(1),controller.GetRawAxis(4));
+    
     
   
   }

a8f7c06f6b9d6e38055a14eab9fa3408b6f20f2b
Ellis Stout <48662841+ElIzstout@users.noreply.github.com>
2021-02-17 08:58:44 -0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,17 @@
 #include <frc>
+using namespace frc;
+class Robot: public TimedRobot{
+  
+  PWMVictorSPX::rightm(1);
+  PWMVictorSPX::leftm(0);
 
-frc:TimedRobot::Timedrobot(){
-
-
-
+  DifferentialDrive::robotdrive(leftm,rightm);
+  XboxController::controller(1); //Lx=0 Ly=1 Lt=2 rt=3 Rx=4 ry=5 
+  
+  teleopPeriodic(){
+    
+    robotdrive.ArcadeDrive(controller.GetRawAxis(1),controller.GetRawAxis(4));
+    
+  
+  }
 }
